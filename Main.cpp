0ccfd5535448294d34d9d740fb500bc10421f780
iTSL4y3R <its.sl4y3r@gmail.com>
2022-01-02 19:29:46 +0900

    Added CMakeLists. Fixed Paths for CMake.

--- Main.cpp
+++ Main.cpp
@@ -33,9 +33,9 @@
 #include <vector>
 #include <functional>
 
-#include "../imgui/imgui.h"
-#include "../imgui/imgui_impl_glfw.h"
-#include "../imgui/imgui_impl_opengl3.h"
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 int main()
 {
